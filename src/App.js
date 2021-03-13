833739aca4fa45e2cd9db06466e9bfc2995f46e2
Hejermejri <hejermejri00@gmail.com>
2021-03-13 18:01:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import myWonderfulImage from "./download.png";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{border:'solid(1px)black',maxwidth:'100vw'}}>
+     <h1 className="titlered">Your name here</h1>
+     <img src={myWonderfulImage}  alt="myimage" />
+     <br/>
+     <img src="/logo-og.png" alt="myimage" />
+     
     </div>
+
   );
 }
 
